0b5d91e90a8a09e7aef8810b7d8312d74917d335
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2022-07-16 19:04:46 +0530

    dsdf

--- config/server.js
+++ config/server.js
@@ -2,7 +2,13 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // keys: env.array('APP_KEYS'),
+    keys: [
+      "EnWbo2UJeLkZWfpVXlebdw==",
+      "Ola/9KoRSxet/AGcYO0qzQ==",
+      "XbnFTHXS+yJpm9hWCwMjjg==",
+      "8UrLES0IBU/VTMon2r1xhw=="
+    ]
   },
   admin: {
     auth: {
