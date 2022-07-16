73f1255f15e3830e5f91cc129671615c4811a64c
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2022-07-16 19:22:02 +0530

    jljl

---
+++ config/plugins.js
@@ -0,0 +1,29 @@
+// path: ./config/plugins.js
+
+module.exports = ({ env }) => ({
+    jwtSecret: env('JWT_SECRET', '0pnf5ShRpZ9JxAhg4FL1Lw==')    
+});
+  
+
+// // path: ./config/plugins.js
+
+// module.exports = ({ env }) => ({
+//     // enable a plugin that doesn't require any configuration
+//     i18n: true,
+  
+//     // enable a custom plugin
+//     myplugin: {
+//       // my-plugin is going to be the internal name used for this plugin
+//       enabled: true,
+//       resolve: './src/plugins/my-local-plugin',
+//       config: {
+//         // user plugin config goes here
+//       },
+//     },
+  
+//     // disable a plugin
+//     myotherplugin: {
+//       enabled: false, // plugin installed but disabled
+//     },
+//   });
+  
