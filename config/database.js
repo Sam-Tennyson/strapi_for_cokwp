a9401fc8283322eabaa213d38052fef5be16b37e
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2022-07-16 18:16:50 +0530

    jkjkjkj

--- config/database.js
+++ config/database.js
@@ -4,12 +4,18 @@ module.exports = ({env}) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", process.env.DATEBASE_PORT),
+      port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "cokwp"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "yahoo@123"),
-      schema: env("DATABASE_PORT", "public")
+      schema: env("DATABASE_PORT", "public"),
+      // ssl: {
+      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      // },
     },
+    // options: {
+    //   ssl: env.bool('DATABASE_SSL', false),
+    // },
   }
 })
 
