e1c2db104881f195dd88c93480cbe457c74f9a7b
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2022-07-16 21:09:12 +0530

    jlkljkjlkjkkjl

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
-      jwtSecret: env("JWT_SECRET"),
+      jwtSecret: "5gbkhrkVogQ4+wAe4kx10KDGYVhE2WbN6zIe2pnR/pGTFP4xVWQ/Ek6C0qWS51taZ0mWa1J8LDbd6PE7jh5uSh2TGuvimVSo0JeyuTPBa430byrqaQTPMu5JwJ24JiX2Hgn6f50s/SZ0/rqxrgJSXKFH9iFOMYZd6dQA8rp0OemYa140417z5MceJzt0tJxrWniipiZYPubqssQBRGrTU97ayjD1a6F2OVoeVu789h3ah+ZDBFCbOF/kSoTR8ja2NDkhE9L5qMazIBdx+jtC/AX/Epi/+fdBsVkS7Rn4q9hql7sIkOgI/Kt36aGemoAJTNPd3IBE7cTtaJ0KZBgTIQ==",
     },
   },
 //   upload: {
